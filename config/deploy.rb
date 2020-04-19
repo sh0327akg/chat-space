40ae326cecfa4bfcdd91b7308499d7a6db1ea6a0
sh0327akg <shota.gk0327m.tf@gmail.com>
2020-04-19 21:43:01 +0900

    変更

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,6 +8,7 @@ set :linked_dirs,fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 't
 set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1'
 set :ssh_options, auth_methods: ['publickey'],keys: ['~/.ssh/chatspace.pem'] 
+set :linked_files,%w{config/secrets.yml}
 
 # プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
@@ -22,8 +23,19 @@ namespace :deploy do
   task :restart do
     invoke 'unicorn:restart'
   end
-end
 
+  desc 'upload secrets.yml'
+  task :upload do
+    on roles(:app) do |host|
+      if test "[ ! -d #{shared_path}/config ]"
+        execute "mkdir -p #{shared_path}/config"
+      end
+      upload!('config/secrets.yml', "#{shared_path}/config/secrets.yml")
+    end
+  end
+  before :starting, 'deploy:upload'
+  after :finishing, 'deploy:cleanup'
+end
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
